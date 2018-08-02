eeedca2f9df1b6b6a414aa051d2b748412c94881
duranhumes <duranhumes@gmail.com>
2018-08-02 11:12:28 -0400

    messages file to help with repeated api responses

---
+++ server/src/lib/helpers/messages.ts
@@ -0,0 +1,3 @@
+export const error500 = { message: 'Something went wrong please try again.' };
+export const error422 = { message: 'Please fill out all fields.' };
+export const error403 = { message: "You don't have access to this." };
