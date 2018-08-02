380263309a8eb8fc0b4bace6554dffed0c677bf9
duranhumes <duranhumes@gmail.com>
2018-08-02 10:34:44 -0400

    moved user related functions closer to user model

--- server/src/controllers/Controller/index.ts
+++ server/src/controllers/Controller/index.ts
@@ -1,6 +1,4 @@
 import * as dotenv from 'dotenv';
-import { getRepository } from 'typeorm';
-import { User } from '../../models';
 
 class Controller {
 	constructor() {
@@ -15,24 +13,6 @@ class Controller {
 
 	public escapeString = (str: string): string => {
 		return escape(String(str));
-	};
-
-	public getUserByEmail = async (email: string): Promise<User | null> => {
-		try {
-			const user = await getRepository(User).findOne({
-				where: { email },
-			});
-
-			if (typeof user === 'undefined') {
-				return null;
-			}
-
-			return user;
-		} catch (error) {
-			console.error(error);
-
-			return null;
-		}
 	};
 }
 
